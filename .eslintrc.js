3d34bb4c245671c620ab60323e44d7c0648c5f3a
Ty Eggen <tycho.eggen@gmail.com>
2021-03-26 18:34:40 -0700

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+    es2017: true
+  },
+  extends: [
+    'standard'
+  ],
+  parserOptions: {
+    ecmaVersion: 12
+  },
+  rules: {
+  }
+}
