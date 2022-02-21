19566d3014e02aa4398d581bcd463e259f2e218d
Ty Eggen <tycho.eggen@gmail.com>
2022-02-21 14:19:26 -0800

    General: improvements - formatting - comments - cleanup - integration

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,13 @@ class PrettyStateMachine {
   consumers: any
   defaultTopic: string
   store: { [k: string]: {} }
+  localStorageKey: string
 
+  /**
+   * Constructor
+   *
+   * @param {string} name
+   */
   constructor (name?: string) {
     this.name = name || 'default'
     this.debug = debug.extend(this.name)
@@ -17,27 +23,76 @@ class PrettyStateMachine {
     this.defaultTopic = 'state'
     this.store = { [this.defaultTopic]: {} }
 
-    if (typeof localStorage !== 'undefined' && localStorage.getItem('pretty-state-machine') !== null) {
-      debug('pretty-state-machine', 'trying to load data from localStorage')
+    this.localStorageKey = 'pretty-state-machine' + ((this.name !== 'default') ? ':' + this.name : '')
 
-      try {
-        const store = JSON.parse(localStorage.getItem('pretty-state-machine'))
-        this.store = { ...store, ...{ [this.defaultTopic]: store } }
-        this.debug('pretty-state-machine', 'loaded data from localStorage')
-      } catch (err) {
-        this.debug('pretty-state-machine', 'failed to load data from localStorage:', err)
+    if (typeof localStorage !== 'undefined') {
+      if (localStorage.getItem(this.localStorageKey) !== null) {
+        this.debug('trying to load data from localStorage')
+
+        try {
+          const store = JSON.parse(localStorage.getItem(this.localStorageKey))
+          this.store = { ...store, ...{ [this.defaultTopic]: store } }
+          this.debug('loaded data from localStorage')
+        } catch (err) {
+          this.debug('failed to load data from localStorage:', err)
+        }
       }
+
+      this.debug('setting up persistence to localStorage for', this.localStorageKey)
+      this.sub((state) => {
+        this.debug('saving to localStorage:', this.localStorageKey, state)
+        localStorage.setItem(this.localStorageKey, JSON.stringify(state))
+      })
     }
+
+    this.pub('init', 'ok')
   }
 
+  /**
+   * Fetch a state as an object
+   *
+   * @param {string} topic
+   * @param {any} defaultVal
+   *
+   * @returns {object}
+   */
   fetch (topic: string, defaultVal: any) {
-    return (this.store[topic] !== undefined) ? { [topic]: this.store[topic] } : (this.store[this.defaultTopic][topic] !== undefined) ? { [topic]: this.store[this.defaultTopic][topic] } : (typeof defaultVal !== 'object') ? { [topic]: defaultVal } : defaultVal || {}
+    defaultVal = (defaultVal) || {}
+
+    /* istanbul ignore next */
+    return (this.store[topic] !== undefined)
+      ? { [topic]: this.store[topic] }
+      : (typeof defaultVal !== 'object')
+          ? { [topic]: defaultVal }
+          : defaultVal[topic] !== undefined
+            ? defaultVal
+            : { [topic]: {} }
   }
 
+  /**
+   * Get a state as a value
+   *
+   * @param topic
+   * @param defaultVal
+   * @returns
+   */
   get (topic: string, defaultVal: any) {
-    return (this.store[topic] !== undefined) ? this.store[topic] : (this.store[this.defaultTopic][topic] !== undefined) ? this.store[this.defaultTopic][topic] : defaultVal || null
+    /* istanbul ignore next */
+    return (this.store[topic] !== undefined)
+      ? this.store[topic]
+      : (this.store[this.defaultTopic][topic] !== undefined)
+          ? this.store[this.defaultTopic][topic]
+          : defaultVal !== undefined
+            ? defaultVal
+            : null
   }
 
+  /**
+   * Set a state
+   *
+   * @param {string} topic
+   * @param {any} value
+   */
   pub (topic: string, args: any) {
     if (typeof topic !== 'string') {
       args = topic
@@ -47,8 +102,12 @@ class PrettyStateMachine {
     let updateObj = {}
 
     if (Array.isArray(args)) {
-      if (this.store[topic] === undefined) this.store[topic] = []
+      /* istanbul ignore next */
+      if (this.store[topic] === undefined) {
+        this.store[topic] = []
+      }
 
+      /* istanbul ignore next */
       if (JSON.stringify(this.store[topic]) !== JSON.stringify(args)) {
         updateObj = { [topic]: args }
       }
@@ -81,6 +140,13 @@ class PrettyStateMachine {
     }
   }
 
+  /**
+   * Subscribe to a state
+   *
+   * @param topic
+   * @param handler
+   * @returns {EventEmitter2}
+   */
   sub (topic: string | any, handler?: any): EventEmitter2 {
     if (typeof topic === 'function') {
       handler = topic
@@ -89,9 +155,21 @@ class PrettyStateMachine {
 
     topic = (topic) || this.defaultTopic
 
-    return this.consumers.on(topic, handler)
+    try {
+      return this.consumers.on(topic, handler)
+    } catch (err) {
+      this.debug('failed to subscribe:', err)
+      return null
+    }
   }
 
+  /**
+   * Unsubscribe from a state
+   *
+   * @param topic
+   * @param handler
+   * @returns
+   */
   unsub (topic: string | any, handler?: any) {
     if (typeof topic === 'function') {
       handler = topic
@@ -102,30 +180,51 @@ class PrettyStateMachine {
     return this.consumers.off(topic, handler)
   }
 
+  /**
+   * Alias for sub
+   *
+   * @param topic
+   * @param handler
+   * @returns
+   */
   attach (topic: string | any, handler?: any) {
     return this.sub(topic, handler)
   }
 
+  /**
+   * Alias for unsub
+   *
+   * @param topic
+   * @param handler
+   * @returns
+   */
   unattach (topic: string | any, handler?: any) {
     return this.unsub(topic, handler)
   }
 
+  /**
+   * Alias for unsub
+   *
+   * @param topic
+   * @param handler
+   * @returns
+   */
   detach (topic: string | any, handler?: any) {
     return this.unsub(topic, handler)
   }
+
+  /**
+   * Shutdown the state machine
+   */
+  shutdown () {
+    this.consumers.removeAllListeners()
+  }
 }
 
+/**
+ * Create a new, default state machine
+ */
 const stateMachine = new PrettyStateMachine()
-
-if (typeof localStorage !== 'undefined') {
-  stateMachine.debug('pretty-state-machine', 'saving to localStorage')
-  stateMachine.sub((store: any) => {
-    stateMachine.debug('pretty-state-machine', 'saved to localStorage')
-    localStorage.setItem('pretty-state-machine', JSON.stringify(store))
-  })
-}
-
-stateMachine.pub('init', 'ok')
 
 export default stateMachine
 export { stateMachine, stateMachine as prettyStateMachine, PrettyStateMachine }
